c8c7597ea6cf8a90cbe8475ebb96297e93671829
Modernize Studio <studio@modernize.com>
2026-09-24 07:57:36 +0000

    Commit for planId: 212

--- src/main/java/com/demo/resortslite/entity/Doctor.java
+++ src/main/java/com/demo/resortslite/entity/Doctor.java
@@ -9,23 +9,23 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="JPA_DOCTOR_INFO")
+@Table(name="jpa_doctor_info")
 public class Doctor {
 
-	@Column(name="DOC_ID")
+	@Column(name="doc_id")
 	@Id
 	//@GeneratedValue(strategy=GenerationType.AUTO)
-	@SequenceGenerator(name="gen1",sequenceName="CNO_SEQ",initialValue = 203,allocationSize = 1)
+	@SequenceGenerator(name="gen1",sequenceName="cno_seq",initialValue = 203,allocationSize = 1)
 	@GeneratedValue(generator ="gen1",strategy = GenerationType.SEQUENCE)
 	private Integer docId;
 
-	@Column(name="DOC_NAME",length=25)
+	@Column(name="doc_name",length=25)
 	private String docName;
 
-	@Column(name="SPECIALIZATION",length=20)
+	@Column(name="specialization",length=20)
 	private String specialization;
 
-	@Column(name="INCOME")
+	@Column(name="income")
 	private Double income;
 
 	public Integer getDocId() {
